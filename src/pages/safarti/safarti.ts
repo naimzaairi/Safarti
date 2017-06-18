68196cbbe2533d2a3b184b8b454df56a37fc3a1f
Naïm Zaaïri <n.zaairi96@gmail.com>
2017-06-18 21:50:43 +0000

    commit 2
    pages mes voyages, safarti (1ere partie),

--- src/pages/safarti/safarti.ts
+++ src/pages/safarti/safarti.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
+import { FirstPage } from '../first/first';
 
 @Component({
   selector: 'page-safarti',
@@ -7,8 +10,28 @@ import { NavController } from 'ionic-angular';
 })
 export class SafartiPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
+  }
+
+  items = [
+    "C'est quoi Safarti ?",
+    'Déconnexion',
+  ];
+
+  itemSelected(item: string) {
+    console.log("Selected Item", item);
+    
+    switch(item){
+      case 'Déconnexion':
+        this.navCtrl.push(FirstPage);
+        break;
+
+      case "C'est quoi Safarti ?":
+        console.log("Salut toi");
+        break;
+
+    }
   }
 
 }
